c69f8bdff79d70b642ff77d6067cbefcd6ec4bca
John Wormald <johnwormald@gmail.com>
2022-09-12 10:26:14 +0100

    Added build folder

--- src/server.js
+++ src/server.js
@@ -2,9 +2,11 @@ import express from 'express';
 import cors from 'cors';
 import bodyParser from 'body-parser';
 import { MongoClient } from 'mongodb';
+import path from 'path';
 
 const app = express();
-const whitelist = ["http://localhost:3000"]
+
+const whitelist = ["http://localhost:3000", "http://localhost:3001"]
 const corsOptions = {
   origin: function (origin, callback) {
     if (!origin || whitelist.indexOf(origin) !== -1) {
@@ -16,6 +18,7 @@ const corsOptions = {
   credentials: true,
 }
 
+app.use(express.static(path.join(__dirname, '/build')));
 app.use(bodyParser.json(), cors(corsOptions));
 
 const withDB = async (operations, res) => {
@@ -30,6 +33,12 @@ const withDB = async (operations, res) => {
         res.status(500).json({ message: 'Error connecting to db', error });
     }
 }
+app.get('/api/articles', async (req, res) => {
+    withDB(async (db) => {
+        const articles = await db.collection('articles').find().toArray();
+        res.status(200).json(articles);
+    }, res);
+})
 
 app.get('/api/articles/:name', async (req, res) => {
     withDB(async (db) => {
@@ -71,6 +80,10 @@ app.post('/api/articles/:name/add-comment', (req, res) => {
 
         res.status(200).json(updatedArticleInfo);
     }, res);
+});
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname + '/build/index.html'));
 });
 
 app.listen(8000, () => console.log('Listening on port 8000'));
